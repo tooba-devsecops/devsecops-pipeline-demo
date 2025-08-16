ae9688518801632d7b69af9bf11e29e2ea599948
Tooba KHAN <tooba.khan8648@gmail.com>
2025-08-16 16:31:56 +0200

    Create main.tf

---
+++ .github/workflows/terraform/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "devsecops-demo-tooba"
+  acl    = "private"
+
+  tags = {
+    Name        = "DevSecOps Demo Bucket"
+    Environment = "Dev"
+  }
+}
