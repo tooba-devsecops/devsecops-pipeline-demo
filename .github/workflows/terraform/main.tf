4bc90cd1a14d7ad75b89374296b55709dd474e31
Tooba KHAN <tooba.khan8648@gmail.com>
2025-08-24 23:46:34 +0200

    Update main.tf

--- .github/workflows/terraform/main.tf
+++ .github/workflows/terraform/main.tf
@@ -1,13 +1,5 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_version = ">= 1.5.0"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "devsecops-demo-tooba"
-  acl    = "private"
-
-  tags = {
-    Name        = "DevSecOps Demo Bucket"
-    Environment = "Dev"
-  }
-}
+provider "null" {}
