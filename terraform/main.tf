cd3214bae58d301942eca4fabee0076301f37aca
Tooba KHAN <tooba.khan8648@gmail.com>
2025-08-24 23:55:17 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,18 @@
 
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "devsecops-demo-tooba"
+  acl    = "private"
+
+  tags = {
+    Name        = "DevSecOps Demo Bucket"
+    Environment = "Dev"
+  }
+}
